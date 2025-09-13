347dbdd793105a9671b178ccf269c3235fa62e74
Jamon <116126138+OnlyPancito@users.noreply.github.com>
2025-09-13 17:49:46 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+from gui import FunctionAnalyzerApp
+
+# Punto de entrada principal para ejecutar la aplicación.
+# Este archivo inicia la interfaz gráfica definida en el módulo 'gui'.
+if __name__ == "__main__":
+    app = FunctionAnalyzerApp()
+    app.mainloop()
